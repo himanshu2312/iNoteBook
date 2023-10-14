21aeac47f47b46f0faf28c3a4b0ee2e471d786d9
himanshu2312 <himanshu2312gupta@gmail.com>
2023-10-14 17:36:50 +0530

    initial setup done concurrently attached

--- src/App.js
+++ src/App.js
@@ -2,8 +2,8 @@ import './App.css';
 
 function App() {
   return (
-    <div>
-      hello
+    <div className='container d-flex justify-content-center'>
+      <h1>This is a NoteBook Application</h1>
     </div>
   );
 }
