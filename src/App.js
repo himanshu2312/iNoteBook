e8eafb68d4ae8910cf21eefa0f9b13cf787d369e
himanshu2312 <himanshu2312gupta@gmail.com>
2023-10-15 17:44:38 +0530

    basic app structure created for app.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
 import './App.css';
+import {BrowserRouter as Router, Routes, Route, Link} from "react-router-dom"
+import NavBar from './Components/NavBar/NavBar';
+import Home from './Components/Home/Home';
+import About from './Components/About/About';
 
 function App() {
   return (
-    <div className='container d-flex justify-content-center'>
-      <h1>This is a NoteBook Application</h1>
-    </div>
+    <>
+      <Router>
+        <NavBar/>
+        <h1>This is a NoteBook Application</h1>
+        <Routes>
+          <Route path="/" Component={Home}/>
+          <Route exact path="/about" Component={About}/>
+        </Routes>
+      </Router>
+    </>
   );
 }
 
